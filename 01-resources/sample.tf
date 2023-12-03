2a8a88c692fa5a6ea10e56c49de6444a60d81155
YMaheswarReddy145 <yettapumahesh398@gmail.com>
2023-12-03 12:04:48 +0530

    Terraform Resource

--- 01-resources/sample.tf
+++ 01-resources/sample.tf
@@ -1,6 +1,5 @@
-resource "aws_instance" "sample"
-{
-  ami           = "ami-03265a0778a880afb"
+resource "aws_instance" "sample" {
+  ami = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
   tags = {
     Name = "Hello World"
