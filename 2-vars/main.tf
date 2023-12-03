763e121bcc60e902a14d7804c20419d9079fa505
YMaheswarReddy145 <yettapumahesh398@gmail.com>
2023-12-03 12:36:01 +0530

    Terraform Variable

--- 2-vars/main.tf
+++ 2-vars/main.tf
@@ -7,9 +7,21 @@ output "sample1" {
 }
 
 variable "fruits" {
-  default = ["apple", "banana", "grapes"]
+  default = ["apple", "banana", "orange"]
 }
 
 output "fruits_first_fruit" {
   value = var.fruits[0]
+}
+
+variable "fruits_with_quantity" {
+  default = {
+    apple = 100
+    banana = 200
+    orange = 300
+  }
+}
+
+output "fruits_apple_orange_quantity" {
+  value = var.fruits_with_quantity["apple"]
 }
