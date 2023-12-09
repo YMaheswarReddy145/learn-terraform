9fa0a2db4935a07a6f23788bd9cc8ce5db2206d9
YMaheswarReddy145 <yettapumahesh398@gmail.com>
2023-12-09 23:30:04 +0530

    Modules in Terraform

--- 04-modules/main.tf
+++ 04-modules/main.tf
@@ -1,3 +1,13 @@
 module "test" {
+  for_each = var.instances
   source = "./test"
+}
+
+# now it will run 3 times because we have provided 3 different instances.,
+variable "instances" {
+  default = {
+    frontend = {}
+    backend = {}
+    mysql = {}
+  }
 }
